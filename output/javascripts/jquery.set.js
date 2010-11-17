5b7ecf0025cd8ec3176be9ca449867a0856a1637
Sergey Ovechkin <me@sovechkin.com>
2010-11-17 16:41:38 +0300

    первый коммит

---
+++ output/javascripts/jquery.set.js
@@ -0,0 +1,28 @@
+(function ($) {
+$.fn.vAlign = function() {
+    return this.each(function(i){
+    var h = $(this).height();
+    var oh = $(this).outerHeight();
+    var mt = (h + (oh - h)) / 2;
+    $(this).css("margin-top", "-" + mt + "px");
+    $(this).css("top", "50%");
+    $(this).css("position", "absolute");
+    });
+};
+$.fn.hAlign = function() {
+    return this.each(function(i){
+    var w = $(this).width();
+    var ow = $(this).outerWidth();
+    var ml = (w + (ow - w)) / 2;
+    $(this).css("margin-left", "-" + ml + "px");
+    $(this).css("left", "50%");
+    $(this).css("position", "absolute");
+    });
+};
+})(jQuery);
+
+$(document).ready(function() {
+    $(".posts").hAlign();
+    $(".commentmeta").fancybox({ 'hideOnContentClick': false });
+	$().geekGaTrackPage('UA-3260762-10');
+});
