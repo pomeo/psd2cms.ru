d514a4374db122b519b51f5e7767b675019d4786
Sergey Ovechkin <me@pomeo.me>
2017-12-16 18:09:25 +0300

    change container ip

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :repository,  "git@github.com:pomeo/#{application}.git"
 #ROLES
 #========================
 set  :gateway,    "#{application}" # main server
-role :app,        "10.3.120.1"      # container
+role :app,        "10.3.10.40"     # container
 
 after "deploy:create_symlink",
       "deploy:cleanup"
